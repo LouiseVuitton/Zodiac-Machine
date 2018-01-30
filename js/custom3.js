78e339d6eb59ebcfd44961713118c59cb334fb32
LouiseVuitton <chanel.lavon@gmail.com>
2018-01-29 22:38:45 -0500

    made changes to Javascript

--- js/custom3.js
+++ js/custom3.js
@@ -1,4 +1,5 @@
-//function triggered by button
+	// //function triggered by button
+
 function signs() {
 	var day = document.getElementById("birthday").value;
 	var month = document.getElementById("birthmonth").value;
@@ -7,9 +8,6 @@ function signs() {
 
 	let h3 = document.querySelector('h3');
 	let h4 = document.querySelector('h4');
-
-	
-
 
 	h3.innerHTML = "Your Birthday: " + month + "/" + day;
 
@@ -31,7 +29,7 @@ function signs() {
 		if (month == 8 && day > 31) {alert("Try Again");}
 		if (month == 9 && day >=23 || month == 10 && day <=22) {value = "Libra";}
 		if (month == 9 && day > 30) {alert("Try Again");}
-		if (month == 10 && day >=23 || month == 11 && day <=21) {value = "Scorpio";}
+		if (month == 10 && day >=23 || month == 11 && day <=21) {value = "Scorpio"; }
 		if (month == 10 && day > 31) {alert("Try Again");}
 		if (month == 11 && day >=22 || month == 12 && day <=21) {value = "Sagittarius";}
 		if (month == 11 && day > 30) {alert("Try Again");}
@@ -40,14 +38,33 @@ function signs() {
 		zodiac = value;
 		console.log(zodiac);
 		h4.innerHTML = "Your sign is " + zodiac;
-		};
 
-// function pic() {
-// 	var pic = document.createElement("IMG");
-// 	pic.setAttribute ("src", "img/aries.png");
-// 	pic.setAttribute ("width", "3.5 rem");
-// 	pic.setAttribute ("height", "3.5 rem");
-// 	pic.setAttribute ("alt", "aries")
+		document.getElementById('btn').onclick = function() {
+            var val = document.getElementById('pic').value,
+                src = 'http://webpage.com/images/' + val +'.png',
+                img = document.createElement('img');
 
-// 	document.body.appendChild(pic);
-// };
+            img.src = src;
+            document.body.appendChild(img);
+        }
+	
+	
+	
+	function symbol () {
+		let img = document.querySelector('img');
+		var repimage = '<img src="';
+		var zodiac = value;
+	
+			if (zodiac == "Aquarius") {
+			repimage += 'https://github.com/LouiseVuitton/Zodiac-Machine/blob/master/img/aquarius.png">';
+			} 
+		else {
+			repimage += 'https://github.com/LouiseVuitton/Zodiac-Machine/blob/master/img/pisces.png>';
+		}
+	
+	
+		img.innerHTML = retvalue;
+	
+	}
+
+}
